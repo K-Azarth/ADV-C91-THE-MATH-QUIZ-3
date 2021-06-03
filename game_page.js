cff97b42ac1875d419f2643cbf5b183edc760e6c
K-Azarth <74723292+K-Azarth@users.noreply.github.com>
2021-06-03 22:37:44 +0530

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -18,8 +18,7 @@ function Send() {
     Number2 = document.getElementById("multiplication2").value;
     equation = parseInt(Number1) * parseInt(Number2);
     console.log(equation);
-
-
+    
     question_number = "<h4>" + number1 + " X " + number2 + "</h4>";
     input_box = "<br>Answer : <input type='text' id='input_check_box'>";
     check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
@@ -29,37 +28,5 @@ function Send() {
     document.getElementById("Number2").value = "";
 }
 
-
 question_turn = "player1";
 answer_turn = "player2";
-
-function check() {
-    get_answer = document.getElementById("input_check_box").value;
-    if (get_answer == equation) {
-        if (answer_turn == "player1") {
-            player1_score = player1_score + 1;
-            document.getElementById("player1_score").innerHTML = player1_score;
-        } else {
-            player2_score = player2_score + 1;
-            document.getElementById("player2_score").innerHTML = player2_score;
-        }
-    }
-
-    if (question_turn == "player1") {
-        question_turn = "player2"
-        document.getElementById("player_question").innerHTML = "Question Turn - " + Player2_name;
-    } else {
-        question_turn = "player1"
-        document.getElementById("player_question").innerHTML = "Question Turn - " + Player1_name;
-    }
-
-    if (answer_turn == "player1") {
-        answer_turn = "player2"
-        document.getElementById("player_answer").innerHTML = "Answer Turn - " + Player2_name;
-    } else {
-        answer_turn = "player1"
-        document.getElementById("player_answer").innerHTML = "Answer Turn - " + Player1_name;
-    }
-
-    document.getElementById("output").innerHTML = "";
-}
